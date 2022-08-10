61e45b1a2cae7b62cd73b967d5f98df75ae2bb0f
mblester <morganmurry@gmail.com>
2022-08-09 21:35:55 -0500

    partial header working

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./App.css";
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div class="header">
+        <header class="SearchHeaderContainer">
+            <div class="SearchHeaderSearch">
+                <span class="SearchHeader_logo"><a href="https://news.ycombinator.com"><img src="//d1sz9gun5ag95e.cloudfront.net/packs/media/images/logo-hn-search-a822432b.png"></img></a></span>
+                    <div class="SearchHeader_label">Search<br></br> Hacker News</div>
+                    <div class="SearchHeader_container"><span class="SearchIcon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FF742A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" background_color="white"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></span>
+                    <input type="search" placeholder="Search stories by title, url or author" class="SearchInput" /></div>
+                    
+                    </div>
+                    <div class="SearchHeader_settings">
+                        <a href="/settings"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg> 
+                        <span class="Settings">Settings</span></a></div>
+                        
+        </header>
+    
+</div>
   )
 }
