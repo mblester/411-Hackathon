77a794e4f67e28203f7e825ec21c8bb062b3d910
Jarrod Bogard <jarrodbogard@gmail.com>
2022-08-17 17:54:13 -0500

    select menu

--- src/App.js
+++ src/App.js
@@ -2,14 +2,57 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Body from "./Body";
+import SelectMenu from "./SelectMenu";
 
 function App() {
   const [list, setList] = useState([]);
   const [searchInput, setSearchInput] = useState("");
+  const [date, setDate] = useState("all");
+  const [timeOrPop, setTimeOrPop] = useState("Popularity");
+  const [type, setType] = useState("story");
+
+  let search = "";
+  let timeStampG = 0;
+  let timeStampL = 0;
+  let tag = type;
+
+  if (timeOrPop === "Date") {
+    search = "search_by_date";
+  } else search = "search";
+
+  if (date === "all") {
+    timeStampL = 0;
+    timeStampG = 31536000000;
+  } else if (date === "last24h") {
+    timeStampL = 0;
+    timeStampG = 86400;
+  } else if (date === "pastWeek") {
+    timeStampL = 86400;
+    timeStampG = 604800;
+  } else if (date === "pastMonth") {
+    timeStampL = 604800;
+    timeStampG = 2630000;
+  } else if (date === "pastYear") {
+    timeStampL = 2630000;
+    timeStampG = 31536000;
+  }
 
   useEffect(() => {
-    if(searchInput){
-      fetch(`http://hn.algolia.com/api/v1/search?query=${searchInput}`)
+    console.log(
+      date,
+      type,
+      timeOrPop,
+      searchInput,
+      search,
+      timeStampL,
+      timeStampG
+    );
+
+    if (searchInput) {
+      fetch(
+        `http://hn.algolia.com/api/v1/${search}?tags=${tag}&query=${searchInput}&numericFilters=created_at_i>${timeStampL}`
+        // `http://hn.algolia.com/api/v1/${search}?tags=${tag}&query=${searchInput}&numericFilters=created_at_i>${timeStampL},created_at_i<${timeStampG}`
+      )
         .then((response) => {
           return response.json();
         })
@@ -18,8 +61,10 @@ function App() {
           setList(data.hits);
         });
     }
-    console.log(searchInput)
-    fetch("http://hn.algolia.com/api/v1/search?tags=front_page")
+    fetch(
+      `http://hn.algolia.com/api/v1/${search}?tags=${tag}&query=${searchInput}&numericFilters=created_at_i>${timeStampL}`
+      // `http://hn.algolia.com/api/v1/${search}?tags=${tag}&query=${searchInput}&numericFilters=created_at_i>${timeStampL},created_at_i<${timeStampG}`
+    )
       .then((response) => {
         return response.json();
       })
@@ -27,12 +72,23 @@ function App() {
         console.log(data.hits);
         setList(data.hits);
       });
-  }, [searchInput]);
+  }, [searchInput, date, type, timeOrPop, tag, search, timeStampG, timeStampL]);
 
   return (
     <div className="App">
       <Header state={searchInput} setState={setSearchInput} />
-      <Body list={list} />
+      <SelectMenu
+        setType={setType}
+        setDate={setDate}
+        setTimeOrPop={setTimeOrPop}
+      />
+      <Body
+        list={list}
+        type={type}
+        date={date}
+        timeOrPop={timeOrPop}
+        setDate={setDate}
+      />
     </div>
   );
 }
--- src/Body.js
+++ src/Body.js
@@ -1,5 +1,5 @@
-import { render } from "@testing-library/react";
 import React from "react";
+import moment from "moment";
 
 export default function Body(props) {
   console.log("props", props);
@@ -7,13 +7,7 @@ export default function Body(props) {
   return (
     <div className="app-container">
       {props.list.map((item, index) => {
-        const dateToStr = item.created_at.toString();
-        const date = new Date(dateToStr);
-        const compareFullYear = date.getUTCFullYear();
-        const currDate = new Date();
-        const currYear = currDate.getFullYear();
-        const renderYears = currYear - compareFullYear;
-        console.log(renderYears);
+        const time = moment(item.created_at).fromNow();
         return (
           <div className="div-display-container" key={index}>
             <article className="story-container">
@@ -25,12 +19,27 @@ export default function Body(props) {
                 </a>
               </div>
               <nav className="story-bar">
-                <a className="" href={item.points}>
+                <a
+                  href={`https://news.ycombinator.com/item?id=${item.objectID}`}
+                >
                   {item.points} points |{" "}
                 </a>
-                <a href={item.author}> {item.author} | </a>
-                <a href={item.created_at}> {renderYears} years ago | </a>
-                <a href={item.num_comments}> {item.num_comments} comments</a>
+                <a href={`https://news.ycombinator.com/user?id=${item.author}`}>
+                  {" "}
+                  {item.author} |
+                </a>
+                <a
+                  href={`https://news.ycombinator.com/item?id=${item.objectID}`}
+                >
+                  {" "}
+                  {time} |{" "}
+                </a>
+                <a
+                  href={`https://news.ycombinator.com/item?id=${item.objectID}`}
+                >
+                  {" "}
+                  {item.num_comments} comments
+                </a>
               </nav>
             </article>
           </div>
@@ -39,3 +48,12 @@ export default function Body(props) {
     </div>
   );
 }
+
+// const dateToStr = item.created_at.toString();
+// const date = new Date(dateToStr);
+// console.log(date, dateToStr, momentDate);
+// const compareFullYear = date.getUTCFullYear();
+// const currDate = new Date();
+// const currYear = currDate.getFullYear();
+// const renderYears = currYear - compareFullYear;
+// console.log(renderYears);
